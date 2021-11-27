088f4b7e7d96b3ad5534fd946d6a8e65716c618e
Anandrazz <90335605+Anandrazz@users.noreply.github.com>
2021-11-27 21:35:29 +0530

    Add files via upload

---
+++ person.js
@@ -0,0 +1,21 @@
+var person1 = {
+    FirstName: "Anand",
+    LastName: "Raj",
+    OfficeAdress: "Madhepura,Bihar 852113",
+  };
+  
+  var person2 = {
+    FirstName: "Abhinav",
+    LastName: "Gautam",
+    OfficeAdress: "Patna, Bihar 800004",
+  };
+  
+  var person3 = {
+    FirstName: "Amrit",
+    LastName: "Kapariya",
+    OfficeAdress: "Khopaiti, Bihar 852121",
+  };
+  
+  console.log(person1);
+  console.log(person2);
+  console.log(person3);
